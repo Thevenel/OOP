263f27832a9ca96b01749fcd1c7b5e5871c57e0d
Thevenel <47800812+Thevenel@users.noreply.github.com>
2023-10-12 09:57:55 +0200

    added courses option

--- student_class.py
+++ student_class.py
@@ -2,10 +2,16 @@
 
 class Student:
 
-    def __init__(self, first, last):
+    def __init__(self, first, last, courses=None):
         self.first_name = first
         self.last_name = last
+        if courses == None:
+            self.courses = ""
+        else:
+            self.courses = courses
 
+courses_1 = ['python', 'rails', 'javascript']
+courses_2 = ['java', 'rails', 'c']
 jay = Student("Thevenel", "Joazard")
 
-print(jay.last_name)
+print(jay.first_name, jay.last_name, jay.courses)
