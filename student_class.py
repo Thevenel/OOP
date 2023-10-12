c56718ffb2dfbddca708c741fccaaf8ebb75a0c1
Thevenel <47800812+Thevenel@users.noreply.github.com>
2023-10-12 10:42:29 +0200

    implemented add_course method

--- student_class.py
+++ student_class.py
@@ -10,8 +10,18 @@ class Student:
         else:
             self.courses = courses
 
+    def add_course(self, course):
+        if course not in self.courses:   
+            self.courses.append(course)
+            print(f"{course} course added successfully for {self.first_name.capitalize()}")
+        else:
+            print(f"{self.first_name.capitalize()} is already enrolled in {course} course")
+    
+
 courses_1 = ['python', 'rails', 'javascript']
 courses_2 = ['java', 'rails', 'c']
-jay = Student("Thevenel", "Joazard")
+jay = Student("Thevenel", "Joazard", courses_1)
+jay.add_course('java')
+jay.add_course('rails')
 
 print(jay.first_name, jay.last_name, jay.courses)
