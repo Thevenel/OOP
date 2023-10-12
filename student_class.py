23962ff952fe65b69501c17010319bb47cf9c63a
Thevenel <47800812+Thevenel@users.noreply.github.com>
2023-10-12 09:48:45 +0200

    created basic student class

---
+++ student_class.py
@@ -0,0 +1,11 @@
+# Student class
+
+class Student:
+
+    def __init__(self, first, last):
+        self.first_name = first
+        self.last_name = last
+
+jay = Student("Thevenel", "Joazard")
+
+print(jay.last_name)
